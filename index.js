ac3e88a0bac5e919cb30c5d4ab8b2d68d8d316ab
LiwaDladloti <liwadladloti@gmail.com>
2017-09-22 12:00:28 +0200

    addeed the routs for getting shoes with brandname and size

--- index.js
+++ index.js
@@ -23,27 +23,83 @@ console.log('We are connected');
 });
 
 var shoes = mongoose.Schema({
-    id: String,
-    brand: String,
-    color: String,
-    size: Number
+//    id : Number,
+    color : String,
+    brand : String,
+    price : Number,
+    size : Number,
+    in_stock : Number
 });
 
 var shoeModel = mongoose.model('shoeApi', shoes);
 
+app.post('/api/shoes', function(req, res){
+    var storeShoes = req.body
+    
+        shoeModel.create({
+            color: storeShoes.color,
+            brand: storeShoes.brand,
+            price: storeShoes.price,
+            size: storeShoes.size,
+            in_stock: storeShoes.in_stock
+        },
+            
+        function(err, results){
+            if(err){
+            return err
+        } else {
+                res.send(results)
+            }
+        })
+});
+
 app.get('/api/shoes', function(req, res){
-        var newShoe = new shoeModel({
-                id: 99,
-                brand: "Nike",
-                color: "Blue",
-                size: 6
-        });
-        newShoe.save();
-    shoeModel.find({}, function(err, results){
+        shoeModel.find({}, function(err, results){
         if(err){
             console.log(err);
         } else {
             res.json(results);
         }
-    });
+    })
+});
+
+app.get('/api/shoes/brand/:brandname', function(req, res){
+    var brandname = req.params.brandname;
+    shoeModel.find({
+        brand: brandname
+    }, function(err, result){
+        if(err){
+            console.log(err)
+        } else {
+            res.json({result})
+        }
+    })
+});
+
+app.get('/api/shoes/size/:size', function(req, res){
+    var size = req.params.size;
+    shoeModel.find({
+        size: size
+    }, function(err, result){
+        if(err){
+            console.log(err)
+        } else {
+            res.json({result})
+        }
+    })
+});
+
+app.get('/api/shoes/brand/:brandname/size/:size', function(req, res){
+    var brandname = req.params.brandname;
+    var size = req.params.size;
+    shoeModel.find({
+        brand: brandname,
+        size: size
+    }, function(err, result){
+        if (err){
+            console.log(err)
+        } else {
+            res.json(result)
+        }
+    })
 });
